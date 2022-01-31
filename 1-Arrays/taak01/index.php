ae2418b0f97629cd180501f045561790d0e9256b
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-01-31 12:53:09 +0100

    taak 2 gedaan van 1

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,6 +1,6 @@
 //Jouw naam,Jouw achternaam,Jouw klas,Jouw leeftijd
 <?php 
 
-$lengte = array("name"=>Stéphano, "Achternaam"=>van Beek, "klas"=>A1, "leeftijd"=>19);
+$studenten = array("name"=>Stéphano, "Achternaam"=>van Beek, "klas"=>A1, "leeftijd"=>19);
 
 ?>
---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,6 @@
+<?php
+//Willem Alexander van Oranje is 53 jaar oud en is student aan het ROC van Amsterdam aan de opleiding Software Ontwikkeling.
+$studenten = array("name"=>Stéphano, "Achternaam"=>van Beek, "opleiding"=>Software Ontwikkeling, "leeftijd"=>19);
+
+echo  "$studenten['voornaam'] $studenten['achternaam'] is $studenten['leeftijd'] jaar oud en is student aan het ROC van Amsterdam aan de opleiding $studenten['Software Ontwikkeling'] .";
+?>

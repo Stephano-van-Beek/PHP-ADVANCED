d43937abf37642dd9b629c2025fa3669e1d283c8
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-02-03 15:52:19 +0100

    foreach gedaan

--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -1,7 +1,12 @@
 <?php
 
-for($x = 0 ; $x < 18; $x++){
+for($x = 0 ; $x <= 18; $x++){
     echo "Ik ben $x jaar, dus ik mag nog niet stemmen <br/>";
 }
-Ik ben 18 jaar oud dus ik heb stemrecht!
+?>
+<?php
+
+for($x = 18 ; $x <= 18; $x++){
+    echo "Ik ben 18 jaar oud dus ik heb stemrecht! <br/>";
+}
 ?>

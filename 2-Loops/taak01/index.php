b0553d6c8a112e55bd72ee0e14a78f7e3ed0d6a5
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-02-03 10:21:08 +0100

    loops gemaakt

--- 2-Loops/taak01/index.php
+++ 2-Loops/taak01/index.php
@@ -1,4 +1,13 @@
 <?php
 
+$leeftijd = 0;
+
+while($leeftijd <18){
+    echo "Ik ben: $leeftijd jaar oud dus ik heb stemrecht! <br/>";
+    $leeftijd++;
+}
+
+//Ik ben x jaar, dus ik mag nog niet stemmen
+//Ik ben 18 jaar oud dus ik heb stemrecht!
 
 ?>
---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,7 @@
+<?php
+
+for($x = 0 ; $x < 18; $x++){
+    echo "Ik ben $x jaar, dus ik mag nog niet stemmen <br/>";
+}
+Ik ben 18 jaar oud dus ik heb stemrecht!
+?>

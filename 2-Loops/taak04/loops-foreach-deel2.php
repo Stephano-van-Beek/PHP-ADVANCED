f043caadca8596342086fde02f678ab92ea3a10c
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-02-04 10:31:13 +0100

    PHP en HTML files gemaakt

---
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -0,0 +1,9 @@
+<?php
+
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+foreach ($klassen as $value) {
+    echo "$value <br>";
+
+?>
+<button type="submit" value= $valeu>
+      Verzenden

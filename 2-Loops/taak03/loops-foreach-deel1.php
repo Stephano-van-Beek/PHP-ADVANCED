e411ae1254c55e054d0146d84a2454d756f56f75
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-02-07 10:57:07 +0100

    dinsdag

--- 2-Loops/taak03/loops-foreach-deel1.php
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$dagen = array("<ul> <li> maandag </ul> </li>", "<ul> <li>dinsadg</ul> </li>", "<ul> <li> woensdag </ul> </li>", "<ul> <li> donderdag </ul> </li>", "<ul> <li> vrijdag </ul> </li>", "<ul> <li> zaterdag </ul> </li>", "<ul> <li> zondag </ul> </li>");
+$dagen = array("<ul> <li> maandag </ul> </li>", "<ul> <li>dinsdagg</ul> </li>", "<ul> <li> woensdag </ul> </li>", "<ul> <li> donderdag </ul> </li>", "<ul> <li> vrijdag </ul> </li>", "<ul> <li> zaterdag </ul> </li>", "<ul> <li> zondag </ul> </li>");
 foreach ($dagen as $value) {
     echo "$value <br>";
 }
--- 2-Loops/taak05/loops-foreach-deel3.php
+++ 2-Loops/taak05/loops-foreach-deel3.php
@@ -1,12 +1,12 @@
 <?php
 
 
-$student  = array(
-    "voornaam"    => "Jan",
-    "achternaam"  => "Janssen",
-    "klas"        => "9A",
-    "Leeftijd"    => 17,
-    "Woonplaats"  => "Amstelveen"
+$student = array(
+    "voornaam"   => "Jan",
+    "achternaam"  => "Janssen",
+    "klas"        => "9A",
+    "Leeftijd"    => 17,
+    "Woonplaats"  => "Amstelveen"
   );
 echo $student
 ?>

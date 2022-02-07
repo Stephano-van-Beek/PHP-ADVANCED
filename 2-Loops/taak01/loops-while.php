dec309ba6432260916a5a9be71514c05c1b786f0
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-02-07 09:13:29 +0100

    index is verplaats naar loops-forech

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -1,13 +1,15 @@
 <?php
 
 $leeftijd = 0;
-
+//Ik ben 18 jaar oud dus ik heb stemrecht!
 while($leeftijd <=18){
     echo "Ik ben: $leeftijd jaar oud dus ik heb stemrecht! <br/>";
     $leeftijd++;
 }
 
 //Ik ben x jaar, dus ik mag nog niet stemmen
-//Ik ben 18 jaar oud dus ik heb stemrecht!
-
+while($leeftijd >18){
+    echo "Ik ben: $leeftijd jaar, dus ik mag nog niet stemmen <br/>";
+    $leeftijd++;
+}
 ?>
--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -1,9 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  
+    <?php 
 
-$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
-foreach ($klassen as $value) {
-    echo "$value <br>";
-}
-?>
-<button type="submit" value= $valeu>;
-      Verzenden
+    $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+    foreach ($klassen as $value) {
+        echo "$value <br>";
+    }
+    ?>
+    <form action="/action_page.php">
+  <label for="cars">Ik zit in klass:</label>
+  <select name="cars" id="cars">
+    <option value="8A">8A</option>
+    <option value="8B">8B</option>
+    <option value="8C">8C</option>
+    <option value="8D">8D</option>
+    <option value="8E">8E</option>
+    <option value="9A">9A</option>
+    <option value="9B">9B</option>
+    <option value="9C">9C</option>
+    <option value="9D">9D</option>
+    <option value="9E">9E</option>
+  </select>
+  <br><br>
+  <input type="submit" value="Submit"> 
+</form>
+</body>
+</html>

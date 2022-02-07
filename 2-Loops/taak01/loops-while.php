ac70ce11cdffa2ec8922774dfd7096686d499921
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-02-07 09:19:22 +0100

    loops is goed

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -1,15 +1,12 @@
 <?php
 
-$leeftijd = 0;
-//Ik ben 18 jaar oud dus ik heb stemrecht!
-while($leeftijd <=18){
-    echo "Ik ben: $leeftijd jaar oud dus ik heb stemrecht! <br/>";
+$leeftijd=0;
+
+while($leeftijd<18){
+    echo "<ul> <li> Ik ben $leeftijd jaar, dus ik mag nog niet stemmen </li> </ul>";
     $leeftijd++;
 }
 
-//Ik ben x jaar, dus ik mag nog niet stemmen
-while($leeftijd >18){
-    echo "Ik ben: $leeftijd jaar, dus ik mag nog niet stemmen <br/>";
-    $leeftijd++;
-}
+echo "<ul> <li>Ik ben 18 jaar oud dus ik heb stemrecht!</li> </ul>"
+
 ?>
